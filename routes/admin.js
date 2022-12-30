5d23eca65d399b310a6bbf5aad49c3d95fd8ab79
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2022-12-30 17:55:02 +0100

    attendance

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 router.post('/login',(req,res)=>{
     const {username,password}= req.body
 
-    if(username === process.env.USERNAME && password === process.env.PASSWORD ){
+    if(username === "admin" && password === "admin" ){
       const token =  jwt.sign({username:username,password:password},process.env.JWT_SECRET,{expiresIn:'30d'})
        return  res.json({token:token})
     }else{
