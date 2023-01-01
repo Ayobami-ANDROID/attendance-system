1d0fa3c984a9fb916603d0273e1390b71c2f09f6
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2023-01-01 19:47:44 +0100

    attendance

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ router.post('/login',(req,res)=>{
     const {username,password}= req.body
 
     if(username === "admin" && password === "admin" ){
-      const token =  jwt.sign({username:username,password:password},process.env.JWT_SECRET,{expiresIn:'30d'})
+      const token =  jwt.sign({username:username,password:password},process.env.JWT_SECRET,{})
        return  res.json({token:token})
     }else{
         return res.status(400).send("wrong username or password")
